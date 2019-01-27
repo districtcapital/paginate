f41683edbb821010e330d6deace6986954ee17f8
Eduardo Pinheiro <edpin@edpin.com>
2019-01-27 15:53:58 -0800

    Adds form parsing to help build a paginate.Query (#8)
    - This helps with Gin requests the most, but is applicable elsewhere too

---
+++ form/form.go
@@ -0,0 +1,125 @@
+// Copyright District Capital Inc 2019
+// All rights reserved.
+
+// Package form helps build a paginate.Query from a web request.
+package form
+
+import (
+	"bytes"
+	"reflect"
+	"strings"
+	"unicode"
+
+	"github.com/districtcapital/paginate"
+)
+
+// ToQuery populates fields in paginate.Query if they are present
+// in req. It looks for fields named PageSize, Page, Select, and OrderBy
+// (exactly) first and copies the values found without interpretation, but
+// converting them to their appropriate types if they fit. Then it looks for
+// annotations "clause" which can be "where" followed or not by an alternate
+// field name (if none is given the snake case of the field is used) and
+// populates the WhereArgs map. If it doesn't find what it's looking for, it
+// does not change Query at all. See tests for examples.
+func ToQuery(req interface{}, q *paginate.Query) {
+	typ := reflect.TypeOf(req).Elem()
+	val := reflect.ValueOf(req).Elem()
+
+	for i := 0; i < typ.NumField(); i++ {
+		typeField := typ.Field(i)
+		structField := val.Field(i)
+
+		switch typeField.Name {
+		case "Page":
+			q.Page = uint32(getUint64(structField))
+			continue
+		case "PageSize":
+			q.PageSize = uint16(getUint64(structField))
+			continue
+		case "OrderBy":
+			q.OrderBy = appendString(q.OrderBy, structField)
+			continue
+		case "Select":
+			q.Select = appendString(q.Select, structField)
+			continue
+		}
+		// Fall through for other fields not recognized by name.
+		// Now we look for `clause:"where"` or variants such as
+		// `clause:"where,age"` and `clause:"where,id"` etc.
+
+		fullClause := strings.Split(strings.ToLower(strings.TrimSpace(typeField.Tag.Get("clause"))), ",")
+		clauseType := fullClause[0]
+		var argName string
+		if len(fullClause) == 2 {
+			argName = strings.TrimSpace(fullClause[1])
+		} else {
+			argName = snakeCase(typeField.Name)
+		}
+		switch clauseType {
+		case "where":
+			if q.WhereArgs == nil {
+				q.WhereArgs = make(map[string]interface{})
+			}
+			if structField.Interface() != reflect.Zero(structField.Type()).Interface() {
+				q.WhereArgs[argName] = structField.Interface()
+			}
+		}
+	}
+}
+
+func getUint64(v reflect.Value) uint64 {
+	switch v.Kind() {
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return uint64(v.Uint())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return uint64(v.Int())
+	}
+	return 0
+}
+
+// appendString appends to slice all strings from v and returns it.
+func appendString(slice []string, v reflect.Value) []string {
+	k := v.Kind()
+	if k == reflect.String {
+		return append(slice, v.String())
+	}
+	if (k == reflect.Slice || k == reflect.Array) && v.Type().Elem().Kind() == reflect.String {
+		for i := 0; i < v.Len(); i++ {
+			slice = append(slice, v.Index(i).String())
+		}
+	}
+	return slice
+}
+
+// snakeCase transforms a CamelCase string into snake_case.
+func snakeCase(s string) string {
+	var buf bytes.Buffer
+
+	if len(s) == 0 {
+		return ""
+	}
+	var last rune
+	w := func(r rune) {
+		last = r
+		buf.WriteRune(unicode.ToLower(r))
+	}
+	w(rune(s[0]))
+	for i := 1; i < len(s); i++ {
+		r := rune(s[i])
+		if r == '_' {
+			w(r)
+			continue
+		}
+		if unicode.IsUpper(r) {
+			if !unicode.IsUpper(last) && last != rune('_') {
+				w(rune('_'))
+				w(rune(s[i]))
+				continue
+			}
+			w(rune(s[i]))
+			continue
+		}
+		w(rune(s[i]))
+	}
+	return buf.String()
+}
---
+++ form/form_test.go
@@ -0,0 +1,109 @@
+// Copyright District Capital Inc 2019
+// All rights reserved.
+
+package form
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/districtcapital/paginate"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPopulate(t *testing.T) {
+	type req struct {
+		Page     int
+		PageSize int
+		ID       int16 `clause:"where"`
+		Age      uint  `clause:"where"`
+		Height   int64 `clause:"where"`
+		Select   string
+		OrderBy  []string
+	}
+	r := &req{
+		Page:     7,
+		PageSize: 25,
+		ID:       99,
+		Age:      37,
+		Height:   550,
+		Select:   "name",
+		OrderBy:  []string{"id", "age"},
+	}
+	q := &paginate.Query{}
+	ToQuery(r, q)
+	assert.Equal(t, uint32(7), q.Page)
+	assert.Equal(t, uint16(25), q.PageSize)
+	if want, got := map[string]interface{}{"age": uint(37), "height": int64(550), "id": int16(99)}, q.WhereArgs; !reflect.DeepEqual(got, want) {
+		t.Fatalf("Where maps do not match: want = %v got = %v", want, got)
+	}
+	assert.Equal(t, []string{"name"}, q.Select)
+	assert.Equal(t, []string{"id", "age"}, q.OrderBy)
+}
+
+func TestPopulateAlternateForm(t *testing.T) {
+	type req struct {
+		Page     uint64
+		PageSize uint32
+		ZZZ      int16 `clause:"where, id"`
+		XXX      *uint `clause:"where,age"`
+		Height   int8  `clause:"where"`
+		Select   []string
+		OrderBy  string
+	}
+	i := uint(69)
+	r := &req{
+		Page:     79,
+		PageSize: 25,
+		ZZZ:      99,
+		XXX:      &i,
+		// Not including field 'Height' will also not include it in the final map.
+		Select:  []string{"one", "two", "three"},
+		OrderBy: "order-me",
+	}
+	q := &paginate.Query{}
+	ToQuery(r, q)
+	assert.Equal(t, uint32(79), q.Page)
+	assert.Equal(t, uint16(25), q.PageSize)
+	x := uint(69)
+	if want, got := map[string]interface{}{"age": &x, "id": int16(99)}, q.WhereArgs; !reflect.DeepEqual(want, got) {
+		t.Fatalf("Where maps do not match, want = %v got = %v", want, got)
+	}
+	assert.Equal(t, []string{"one", "two", "three"}, q.Select)
+	assert.Equal(t, []string{"order-me"}, q.OrderBy)
+}
+
+func TestPopulateInvalid(t *testing.T) {
+	type req struct {
+		Page     string
+		PageSize []byte
+		OrderBy  map[string]string
+		Selectme interface{} `clause:"select"`
+	}
+	r := &req{
+		Page:     "10",
+		PageSize: []byte("32"),
+		OrderBy:  map[string]string{"age": "meh"},
+		Selectme: "foo",
+	}
+	q := &paginate.Query{}
+	ToQuery(r, q)
+	// Nothing was populated.
+	assert.Equal(t, &paginate.Query{}, q)
+}
+
+func TestSnakeCase(t *testing.T) {
+	assert.Equal(t, "", snakeCase(""))
+	assert.Equal(t, "id", snakeCase("ID"))
+	assert.Equal(t, "user_id", snakeCase("UserID"))
+	assert.Equal(t, "user_id", snakeCase("UserId"))
+	assert.Equal(t, "http", snakeCase("HTTP"))
+	assert.Equal(t, "httpdirectory", snakeCase("HTTPDirectory"))
+	assert.Equal(t, "httpdirectory_id", snakeCase("HTTPDirectoryID"))
+	assert.Equal(t, "copy_url", snakeCase("CopyURL"))
+	assert.Equal(t, "path_url", snakeCase("PathUrl"))
+	assert.Equal(t, "funky_string___yo", snakeCase("Funky_String___yo"))
+	// Either we make this one be nice ("copy_url_to_id") or we have to
+	// special case things like "UserId" and "PathUrl".
+	assert.Equal(t, "copy_urlto_id", snakeCase("CopyURLtoID"))
+}
